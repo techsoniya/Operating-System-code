0ff54e7ea81322c0972ff9d9c53e2c9c7dd35375
Soniya Kambli <100508652+techsoniya@users.noreply.github.com>
2023-08-09 22:37:21 +0530

    Update 2fcfs.c

--- 2fcfs.c
+++ 2fcfs.c
@@ -3,31 +3,21 @@
 int main()
 {
     int RQ[100],i,n,TotalHeadMoment=0,initial;
-    printf("Enter the number of Requests");
+    printf("Enter the number of Requests\n");
     scanf("%d",&n);
-    printf("Enter the requests sequence \n");
+    printf("Enter the request sequence\n");
     for(i=0;i<n;i++)
-       scanf("%d",&RQ[i]);
-    printf("enter initial head position \n");
+    scanf("%d",&RQ[i]);
+    printf("Enter the initial head position\n");
     scanf("%d",&initial);
-    //logic for FCFS disk schdeuling
-    printf("Sequence of request access: \n");
+    printf("Sequence of request access:\n");
     for(i=0;i<n;i++)
     {
-        printf("%d  ",RQ[i]);
+        printf("%d",RQ[i]);
         TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
         initial=RQ[i];
     }
-    printf("\n Total head Moment is %d  ",TotalHeadMoment);
+    printf("\n Total head moment id %d",TotalHeadMoment);
     return 0;
 }
-/*
-Enter the number of Requests
-7
-Enter the requests sequence 
-82 170 43 140 24 16 190 
-enter initial head position 
-50
-Sequence of request access: 
-82 170 43 140 24 16 190
- Total head Moment is 642  [1] + Done*/
+
