78a5952af3b3bbf100b8628e76d53fefba61c494
Soniya Kambli <100508652+techsoniya@users.noreply.github.com>
2023-08-09 22:37:59 +0530

    Update 2fcfs.c

--- 2fcfs.c
+++ 2fcfs.c
@@ -2,22 +2,27 @@
 #include<stdlib.h>
 int main()
 {
-    int RQ[100],i,n,TotalHeadMoment=0,initial;
-    printf("Enter the number of Requests\n");
+    int rq[100],i,j,n,totalheadmoment=0,initial, size , move;
+    printf("Enter the number of requests\n");
     scanf("%d",&n);
     printf("Enter the request sequence\n");
     for(i=0;i<n;i++)
-    scanf("%d",&RQ[i]);
-    printf("Enter the initial head position\n");
+    {
+        scanf("%d",&rq[i]);
+    }
+    printf("Enter the inital head position\n");
     scanf("%d",&initial);
-    printf("Sequence of request access:\n");
+    printf("Enter total disk size\n");
+    scanf("%d",&size);
+    printf("Enter the head movement direction for high 1 and low 0\n");
+    scanf("%d",&move);
     for(i=0;i<n;i++)
     {
-        printf("%d",RQ[i]);
-        TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
-        initial=RQ[i];
+        for(j=0;j<n-i-1;j++)
+        {
+            int temp;
+            temp = rq[j];
+            rq[j]= rq[j+1];
+            rq[j+1]=temp;
+        }
     }
-    printf("\n Total head moment id %d",TotalHeadMoment);
-    return 0;
-}
-
